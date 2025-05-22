2cdb3b4c68a9213ef2b2edb4f1596a6dc5ae42ff
Marttico <g.martti@gmail.com>
2025-05-22 23:37:10 +0200

    This is being stupid, add it on windows.

--- src/led_effects_driver_priv.hpp
+++ src/led_effects_driver_priv.hpp
@@ -1,12 +1,9 @@
-#include "led_strip.h"
-#include "freertos/task.h"
-#include <vector>
 
-led_strip_handle_t led_strip = NULL;
-uint32_t led_amount;
-uint8_t brightness;
-uint8_t fps;
-TaskHandle_t xLedTaskHandle = NULL;
+#include "freertos/task.h"
+#include "led_strip.h"
+#include <vector>
+#include <string>
+
 
 struct led_pixel_t {
     uint8_t red;
@@ -14,25 +11,73 @@ struct led_pixel_t {
     uint8_t blue;
 };
 
-enum led_animation_t{
-    LED_ANIMATION_RIPPLE,
-    LED_ANIMATION_HEARTBEAT,
-    LED_ANIMATION_WIPE,
-    LED_ANIMATION_RAINBOW,
-    LED_ANIMATION_SINGLE_FLASH
-};
-
-class led_animation_queue_entry {
-    private:
-        uint8_t red, green, blue;
-        uint32_t time_length;
-        enum led_animation_t animation_type;
-    public:
-        led_animation_queue_entry(uint32_t time_length, enum led_animation_t anim_type);
-        led_animation_queue_entry(uint8_t r, uint8_t g, uint8_t b, uint32_t t_length, enum led_animation_t anim_type);
-        void led_animation_process(led_pixel_t** strip, uint32_t timetick);
-
-};
-
 
 void led_manager_trigger_handler(void* arg);
+
+
+void process_led_effects_queue(led_pixel_t** strip,uint64_t timetick);
+
+// enum led_animation_t{
+//     LED_ANIMATION_RIPPLE,
+//     LED_ANIMATION_HEARTBEAT,
+//     LED_ANIMATION_WIPE,
+//     LED_ANIMATION_RAINBOW,
+//     LED_ANIMATION_SINGLE_FLASH
+// };
+
+class led_animation {
+    private:
+        std::string name;
+        uint32_t time_length;
+        uint32_t current_time;
+        std::vector<led_animation> *animation_queue;
+
+    public:
+        led_animation(uint32_t t_l, std::vector<led_animation>* anim_q;){
+            animation_queue = anim_q;
+            time_length = t_l;
+            current_time = 0;
+        }
+        
+        std::string getName(){
+            return name;
+        }
+        
+        void removeFromQueue(){
+            animation_queue;
+        }
+
+        virtual void processAnimation(led_pixel_t** strip, uint32_t timetick) = 0;
+
+
+}
+
+class led_ripple : public led_animation {
+    private:
+        uint8_t red, green, blue;
+
+    public:
+    led_ripple(uint32_t t_l, std::string nm, uint8_t r, uint8_t g, uint8_t b) : led_animation(t_l, nm){
+        red = r;
+        green = g;
+        blue = b;
+    }
+
+    void processAnimation(led_pixel_t** strip, uint32_t timetick){
+
+    }
+
+}
+
+
+// class led_animation_queue_entry {
+//     private:
+//         uint8_t red, green, blue;
+//         uint32_t time_length;
+//         led_animation_t animation_type;
+//     public:
+//         led_animation_queue_entry(uint32_t time_length, enum led_animation_t anim_type);
+//         led_animation_queue_entry(uint8_t r, uint8_t g, uint8_t b, uint32_t t_length, enum led_animation_t anim_type);
+//         void led_animation_process(led_pixel_t** strip, uint32_t timetick);
+
+// };
--- src/led_effects_queue.cpp
+++ src/led_effects_queue.cpp
@@ -1,10 +1,12 @@
 #include "led_effects_driver.hpp"
 #include "led_effects_driver_priv.hpp"
 
-std::vector<led_animation_queue_entry> animation_queue;
+#define TAG "led_effects_queue"
+
+std::vector<led_animation> animation_queue;
 
 
-void led_effects_add_to_queue(led_animation_queue_entry entry){
+void led_effects_add_to_queue(led_animation entry){
     animation_queue.push_back(entry);
 }
 
@@ -14,10 +16,12 @@ void led_effects_remove_from_queue(){
 
 
 // Process each queued animation. Make sure you delete the ones that are finished.
-void process_led_effects_queue(led_pixel_t** strip,uint32_t timetick){
+void process_led_effects_queue(led_pixel_t** strip,uint64_t timetick){
 
-    for(led_animation_queue_entry d : animation_queue){
+    for(led_animation anim : animation_queue){
         
+
+        ESP_LOGI(TAG,"%s",anim.getName().c_str());
     }
 
 }
